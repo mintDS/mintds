00d96c7277daf3cba3851a3071c7ba7b3e1f8ff7
Artur Mkrtchyan <artur.mkrtchyan@gamegenetics.de>
2015-10-21 22:47:53 +0200

    Optimized ServerInitializer

--- src/main/java/com/arturmkrtchyan/mintds/server/ServerInitializer.java
+++ src/main/java/com/arturmkrtchyan/mintds/server/ServerInitializer.java
@@ -3,25 +3,35 @@ package com.arturmkrtchyan.mintds.server;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
 
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
 
+    private final StringDecoder stringDecoder = new StringDecoder(CharsetUtil.UTF_8);
+    private final StringEncoder stringEncoder = new StringEncoder(CharsetUtil.UTF_8);
+    private final MessageDecoder messageDecoder = new MessageDecoder();
+    private final ServerHandler serverHandler = new ServerHandler();
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
 
         ChannelPipeline pipeline = ch.pipeline();
 
-        // Add the number codec first,
-        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+        // decoders
+        // Add the text line codec combination first,
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast("stringDecoder", stringDecoder);
+        pipeline.addLast("messageDecoder", messageDecoder);
 
-        pipeline.addLast(new MessageDecoder());
+        // encoders
+        pipeline.addLast("stringEncoder", stringEncoder);
 
-        pipeline.addLast(new ServerHandler());
+        // business logic handler
+        pipeline.addLast("serverHandler", serverHandler);
 
     }
 }
