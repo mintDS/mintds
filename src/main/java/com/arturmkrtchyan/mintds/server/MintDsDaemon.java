27116dfd26d21d2f7c0be5528cbbd99b4b89fae0
Artur Mkrtchyan <artur.mkrtchyan@gamegenetics.de>
2015-10-23 16:56:15 +0200

    Added logger into the project

--- src/main/java/com/arturmkrtchyan/mintds/server/MintDsDaemon.java
+++ src/main/java/com/arturmkrtchyan/mintds/server/MintDsDaemon.java
@@ -1,16 +1,23 @@
 package com.arturmkrtchyan.mintds.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.arturmkrtchyan.mintds.server.netty.NettyServer;
 
 public class MintDsDaemon {
 
+    private final static Logger logger = LoggerFactory.getLogger(MintDsDaemon.class);
+
     public static void main(final String[] args) {
         final MintDsServer server = new NettyServer();
+        logger.info("Starting MintDS Server.");
         server.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
+                System.out.println("Stopping Server");
                 server.stop();
             }
         });
