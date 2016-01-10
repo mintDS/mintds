c0ebe318cb2d929e16812400d5a7addae43d32c0
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2016-01-10 20:47:26 +0100

    Added counting bloom filter implementation

---
+++ src/main/java/com/arturmkrtchyan/mintds/core/CountingBloomFilterStore.java
@@ -0,0 +1,28 @@
+package com.arturmkrtchyan.mintds.core;
+
+import com.arturmkrtchyan.mintds.core.datastructure.CountingBloomFilter;
+import com.arturmkrtchyan.mintds.protocol.request.Request;
+import com.arturmkrtchyan.mintds.protocol.response.EnumResponse;
+import com.arturmkrtchyan.mintds.protocol.response.Response;
+
+class CountingBloomFilterStore extends AbstractKeyValueStore<CountingBloomFilter> {
+
+    public CountingBloomFilterStore() {
+    }
+
+    @Override
+    public void add(final CountingBloomFilter filter, final Request request) {
+        filter.add(request.getValue().get());
+    }
+
+    @Override
+    public Response contains(final CountingBloomFilter filter, final Request request) {
+        return filter.isPresent(request.getValue().get()) ? EnumResponse.YES : EnumResponse.NO;
+    }
+
+    @Override
+    protected CountingBloomFilter newElement() {
+        return new CountingBloomFilter(1,1);
+    }
+
+}
