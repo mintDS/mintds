d3d9b08fa908de236d9256da36fe00cd4383ca3c
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2016-01-19 22:57:32 +0100

    Added count operation into countingbloomfilter

--- src/main/java/com/arturmkrtchyan/mintds/core/CountingBloomFilterStore.java
+++ src/main/java/com/arturmkrtchyan/mintds/core/CountingBloomFilterStore.java
@@ -3,6 +3,7 @@ package com.arturmkrtchyan.mintds.core;
 import com.arturmkrtchyan.mintds.core.datastructure.CountingBloomFilter;
 import com.arturmkrtchyan.mintds.protocol.request.Request;
 import com.arturmkrtchyan.mintds.protocol.response.EnumResponse;
+import com.arturmkrtchyan.mintds.protocol.response.NumericResponse;
 import com.arturmkrtchyan.mintds.protocol.response.Response;
 
 class CountingBloomFilterStore extends AbstractKeyValueStore<CountingBloomFilter> {
@@ -22,13 +23,18 @@ class CountingBloomFilterStore extends AbstractKeyValueStore<CountingBloomFilter
     }
 
     @Override
+    public Response count(final CountingBloomFilter filter, final Request request) {
+        return new NumericResponse<>(filter.count(request.getValue().get()));
+    }
+
+    @Override
     public Response contains(final CountingBloomFilter filter, final Request request) {
         return filter.isPresent(request.getValue().get()) ? EnumResponse.YES : EnumResponse.NO;
     }
 
     @Override
     protected CountingBloomFilter newElement() {
-        return new CountingBloomFilter(1,1);
+        return new CountingBloomFilter(1024, 1d);
     }
 
 }
